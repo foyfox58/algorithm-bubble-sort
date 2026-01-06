71de904a19d08df1c3645c222684110afabc1cc5
foyfox58 <foy9323@gmail.com>
2026-01-06 10:51:20 +0700

    feat: implement sorting functions for numbers and student scores

--- th/ex-1.js
+++ th/ex-1.js
@@ -7,6 +7,28 @@
 */
 
 // Start coding here
+function sortedFromMinToMax(numbers){
+    const n = numbers.length; // หาจำนวนสมาชิกก่อน
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (numbers[j] > numbers[j + 1]) {
+        let temp = numbers[j]; // temp -> ตัวเก็บค่าชั่วคราว สมมติ j คือ 64 j+1 จะเป็น 34
+        // temp = 64;
+        numbers[j] = numbers[j + 1];
+        // reassign ค่า -> เพื่อที่จะสลับตำแหน่ง [64, 34]
+        // numbers[0] = 34;
+        // [64, 34] -> [34, 34]
+        numbers[j + 1] = temp;
+        // reassign ค่า -> เพื่อสลับตำแหน่ง
+        // numbers[1] = 64
+      }
+    }
+  }
+  return numbers;
+}
 
 const numbers = [64, 34, 25, 12, 22, 11, 90];
 console.log(sortedFromMinToMax(numbers)); // [11, 12, 22, 25, 34, 64, 90]
+
+// .sort() -> abstract -> software concept
